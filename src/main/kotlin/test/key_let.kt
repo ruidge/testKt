571827bf3660ee1c537c81514fbe682a5ab508c8
rui.zhang1 <rui.zhang1@dmall.com>
2023-01-18 16:59:13 +0800

    update

--- src/main/kotlin/test/key_let.kt
+++ src/main/kotlin/test/key_let.kt
@@ -4,19 +4,25 @@ package test
 fun main() {
 
     var value: String? = "123"
-    value = null
+//    value = null
 
+    //两者都可实现类似 if (value != null) {}else{}的逻辑,
+    //第一种需要确定第一块最后一行的返回值为非null,避免走进第二块.(Unit也算非null)
+    //第二种需要确定第一块最后一行的返回值为null,避免走进第二块.
     value?.let {
         println("not null")
         funTest()
+        Unit
     } ?: run {
         println("is null")
     }
     println("----------------------")
-    value ?: run {
+    (value ?: run {
         println("is null")
         null
-    }?.let { println("not null") }
+    })?.let {
+        println("not null")
+    }
 }
 
 fun funTest(): Int? {
