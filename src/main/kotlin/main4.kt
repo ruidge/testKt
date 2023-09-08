a93eb8cddff49cfc9f839310fa39f8e85c1a0d75
rui.zhang1 <rui.zhang1@dmall.com>
2023-09-08 16:52:03 +0800

    update

---
+++ src/main/kotlin/main4.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var value: String? = "123"
+    value = ""
+    value = null
+
+    val str = "$value"
+
+    println(str)
+    println(value)
+
+}
--- src/main/kotlin/test2/t_gson.kt
+++ src/main/kotlin/test2/t_gson.kt
@@ -11,16 +11,23 @@ import com.google.gson.Gson
 
 fun main() {
     val gson = Gson()
-    val str = "{\"name\":\"zhang\",\"age\":\"12\",\"name1\":\"name1\",\"name2\":\"name2\"}"
+    val str = "{\"name\":\"rui.zhang\",\"age\":\"12\",\"name1\":\"name1\",\"name2\":\"name2\"}"
     var o = gson.fromJson(str, Obj::class.java)
     println(gson.toJson(o))
     println(o.name1)
     println(o.name2)
+    o.age = "22"
+    o.printAge()
 }
 
-class Obj {
-    var name: String? = null
+open class ObjP {
     var age: String? = null
+}
+
+class Obj : ObjP() {
+    //private可以被gson反序列化
+    private var name: String? = null
+
     //如果下发null,则为null;如果没下发,则为false
     var isMale: Boolean? = false
 
@@ -31,5 +38,9 @@ class Obj {
     //var的会作为属性,会被gson反序列化,不过get的时候会转为别的值,如果get返回自己会死循环
     var name2: String? = null
         get() = "${name}2"
+
+    fun printAge() {
+        println("$age")
+    }
 
 }
