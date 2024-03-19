ba87ff2f41370b06bcc2a86a8ccee74445f82616
rui.zhang1 <rui.zhang1@dmall.com>
2024-03-19 14:23:30 +0800

    update

--- src/main/kotlin/test/t_func.kt
+++ src/main/kotlin/test/t_func.kt
@@ -12,6 +12,7 @@ fun main() {
     printGson(array.filter { it >= 5 })
     //过程会生成2个列表
     printGson(array.map { it * 2 }.filter { it >= 5 })
-    //asSequence只生成1个列表
-    printGson(array.asSequence().map { it * 2 }.filter { it >= 5 }.toList())
+    //asSequence只生成1个列表,调用.toList()才生成列表
+    printGson(array.asSequence().map { print("map($it) "); it * 2 }
+        .filter { print("filter($it) "); it <= 8 }.toList())
 }
